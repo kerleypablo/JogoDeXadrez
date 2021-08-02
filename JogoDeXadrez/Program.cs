74b1424ea7c787e30e31a4d2ceb4252d7accbb9c
kerley pablo <kerley.pablo@hotmail.com>
2021-08-02 13:59:14 -0300

    teste comit

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -6,7 +6,7 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("teste");
+            Console.WriteLine("teste 2");
         }
     }
 }
